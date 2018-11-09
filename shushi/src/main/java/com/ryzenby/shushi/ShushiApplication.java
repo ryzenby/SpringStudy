b48ec8d9664667c67494cf6ab8009d2c7c5810fc
ryzenby <ryzenby@163.com>
2018-11-09 17:29:40 +0800

    2018年11月9日

---
+++ shushi/src/main/java/com/ryzenby/shushi/ShushiApplication.java
@@ -0,0 +1,12 @@
+package com.ryzenby.shushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShushiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShushiApplication.class, args);
+    }
+}
